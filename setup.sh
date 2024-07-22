1fbae3aff38b6a5009373aacb18945a268de1034
memebot50000 <164225944+memebot50000@users.noreply.github.com>
2024-07-22 14:50:07 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update && sudo apt-get upgrade
+echo "Installing basic dependencies"
+sudo apt install terminator git python3-pip -y 
+sudo apt-get install build-essential -y 
+echo "Getting ready to install ROS2 Humble"
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale #verify settings
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt upgrade
+echo "Installing ROS2 Humble"
+sudo apt install ros-humble-desktop
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+pip install -U colcon-common-extensions
