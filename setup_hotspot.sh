30aa981be4689e766bee102b446482ca38ddc865
memebot50000 <164225944+memebot50000@users.noreply.github.com>
2024-07-29 15:23:45 -0400

    Update setup_hotspot.sh

--- setup_hotspot.sh
+++ setup_hotspot.sh
@@ -50,18 +50,16 @@ nmcli connection modify DeltaV wifi-sec.psk "12341234"
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
-# Configure NAT
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-
-# Save iptables rules
-iptables-save > /etc/iptables.ipv4.nat
+# Disable firewall
+ufw disable
+iptables -F
+iptables -t nat -F
+iptables -X
+iptables -t nat -X
 
 # Create startup script
 cat > /usr/local/bin/start_hotspot.sh << EOF
 #!/bin/bash
-iptables-restore < /etc/iptables.ipv4.nat
 nmcli con up DeltaV
 EOF
 
